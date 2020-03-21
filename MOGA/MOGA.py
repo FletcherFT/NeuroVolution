70d4eb78422abfe00e0348ad2e4ed524686dad26
FletcherFT <fletcherthompsonx@gmail.com>
2020-03-21 09:44:02 +0100

    First working implementation of MOGA acting on Zermelo's Navigation Problem via Neural Network.

--- MOGA/MOGA.py
+++ MOGA/MOGA.py
@@ -3,19 +3,88 @@ import random
 
 
 class NDSAGA:
-    def __init__(self, pop_size, elitism=0.1):
+    def __init__(self, pop_size, elitism=0.9, mutation_rate=0.02):
+        # Number of solutions
         self._n = pop_size
-        self._e = elitism
+        # Number of solutions to keep
+        self._e = int(pop_size*elitism)
+        # The mutation rate default from https://arxiv.org/pdf/1712.06567.pdf
+        self._m = mutation_rate
+        # Make sure the elitism is between 1 and N-1
+        if self._e >= self._n:
+            self._e = self._n - 1
+        if self._e < 1:
+            self._e = 1
 
-    def _ndsa(self, solutions):
-        # unpack the solutions
-        idx, x, z = zip(*solutions)
+    def _ndsa(self, fitnesses):
+        """Take in a list of fitnesses, find the non dominated solutions.
+        Input:
+        fitnesses: A MxN numpy array, each row is a solution, each column is a fitness.
+        Outputs:
+        An ordered list, each element contains a list of row indices of fitnesses that correspond
+        to a non-dominated ranking: 0 - 1st rank, 1 - 2nd rank and so on.
+        1st rank is completely non-Dominated (the Pareto front)."""
+        # Dominated Counter
+        d = np.zeros((self._n, 1))
+        # Dominating Tracker, a M length list showing what solutions are dominated by a solution.
+        s = [[] for _ in range(self._n)]
+        # The current front ranking (initialised at 0)
+        f = 0
+        # Rankings list (theoretically there can be up to M rankings)
+        F = [[] for _ in range(self._n)]
         # For every solution, check if it dominates the rest
         for i in range(self._n):
+            # select solution p
+            p = fitnesses[i, :]
             for j in range(i + 1, self._n):
-                pass
+                # select solution q
+                q = fitnesses[j, :]
+                # If p dominates q
+                if np.all(p <= q) and np.any(p < q):
+                    # Increase the domination counter of q
+                    d[j] = d[j] + 1
+                    # Add index of q to s[i]
+                    s[i].append(j)
+                # If q dominates p
+                elif np.all(q <= p) and np.any(q < p):
+                    # Increase the domination counter of p
+                    d[i] = d[i] + 1
+                    # Add index of p to s[j]
+                    s[j].append(i)
+            # If solution p is non-dominated, then assign first non-dominated rank (0 indexed)
+            if d[i] == 0:
+                F[f].append(i)
+        # Loop through solutions to find the non-dominated points
+        while len(F[f]) > 0:
+            # For each solution in rank f
+            for i in F[f]:
+                # For each solution dominated by i
+                for j in s[i]:
+                    d[j] = d[j] - 1
+                    if d[j] == 0:
+                        F[f+1].append(j)
+            # Increment the rank
+            f = f + 1
+        # Remove empty rankings from F and return
+        return [i for i in F if len(i) > 0]
 
-    def update(self, fitness):
-        pass
+    def _mutate(self, weights):
+        return [weight + self._m * np.random.randn() for weight in weights]
 
-
+    def update(self, solutions):
+        # Step 1: calculate fitness step has already been done.
+        weights = [i[1] for i in solutions]
+        fitnesses = np.array([i[2:] for i in solutions])
+        # Step 2: identify the Pareto rankings
+        rankings = self._ndsa(fitnesses)
+        # Unroll the rankings
+        rankings = [j for i in rankings for j in i]
+        # Step 3: Survival
+        # indices for solutions that survive to be mutated
+        parents = rankings[:self._e]
+        children = rankings[self._e:]
+        # Step 4: Mutation
+        for i in range(len(children)):
+            weights[children[i]] = self._mutate(weights[random.choice(parents)])
+        # Return the new solutions
+        return weights
--- MOGA_Zermelo.py
+++ MOGA_Zermelo.py
@@ -1,74 +1,51 @@
 from multiprocessing import Process, cpu_count
 from multiprocessing import JoinableQueue as Queue
 from ZermeloSim.ZermeloSim import worker
-import numpy as np
+from MOGA.MOGA import NDSAGA
 import random
 
 
-def get_top_10(results):
-    results = np.array(results)
-    sort_idx = np.argsort(results, axis=0)
-    dist_idx = sort_idx[:, 1]
-    n = len(dist_idx)
-    top10 = int(max((float(n) * 0.1), 1))
-    return dist_idx[:top10].tolist()
-
-
-def mutate(agent):
-    child_agent = Agent()
-    mutation_power = 0.02  # hyper-parameter, set from https://arxiv.org/pdf/1712.06567.pdf
-    weights = agent.model.get_weights()
-    weights = [weight + mutation_power*np.random.randn() for weight in weights]
-    child_agent.model.set_weights(weights)
-    return child_agent
-
-
 if __name__ == '__main__':
+    # Number of generations
+    G = 1000
+    # Simulation length (iterations)
+    I = 1000
+    # population size
     n = 100
+    # number of simulations to run at once
     p = cpu_count()-1
+    # set up jobs queue for simulation processes
     jobs = Queue()
+    # set up results queue for simulation processes
     results = Queue()
-    pool = [Process(target=worker, args=(jobs, results)).start() for _ in range(p)]
+    # Start the simulation processes
+    pool = [Process(target=worker, args=(jobs, results, I)).start() for _ in range(p)]
+    # Get the indices
     idx = range(n)
+    # Initialise the weights
     weights = [-1]*n
+    MOGA = NDSAGA(n, 0.9, 0.02)
+    # Begin MOGA Loop Here
+    for g in range(G):
+        # STEP 1: GET FITNESSES
+        # Place the jobs into the simulation queue
+        for job in zip(idx, weights):
+            jobs.put(job)
+        #jobs.join()
+        # Wait until all simulations are done
+        r = [results.get() for _ in range(n)]
+        # Sort the results
+        r.sort()
+        # STEP 2: GET SOLUTION UPDATES
+        weights = MOGA.update(r)
+    # The final generation:
+    # Place the jobs into the simulation queue
     for job in zip(idx, weights):
         jobs.put(job)
+    # Wait until all simulations are done
     jobs.join()
     r = [results.get() for _ in range(n)]
+    # Close the processes
     for _ in range(p):
         jobs.put(None)
     jobs.join()
-
-
-
-if __name__ == "__main__":
-    # TODO Set up MOGA.
-    # The population size
-    N = 10
-    # Initialise the simulator
-    #S = Simulator(N, num_workers=100)
-
-    S = SimulatorMP(N, num_proc=1)
-    # Number of generations
-    G = 1
-    for i in range(G):
-        # Run the simulation, and get the results
-        results = S.run_sim()
-        # Get the top 10 performers
-        top10 = get_top_10(results)
-        # Get the bottom 90 performers
-        bottom90 = list(set(range(N)) - set(top10))
-        # Get the list of agents
-        agents = S.agents
-        # Modify the remainder
-        elites = [agents[t] for t in top10]
-        # Replace the members of bottom90 with a mutation of top10
-        for idx in bottom90:
-            agents[idx] = mutate(random.choice(elites))
-        # Update the agents in the simulator
-        S.agents = agents
-        # Reset the environment
-        S.reset()
-        idx, distance, energy, time = zip(*results)
-        best = top10[0]
-        print("{} Best\t{}".format(i, results[best]))
